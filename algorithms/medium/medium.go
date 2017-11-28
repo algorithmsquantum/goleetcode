af4c9a3cdad749c5680c8f17fa9acd4cf057c01f
Lingchao Xin <douglarek@gmail.com>
2017-11-28 21:09:37 +0800

    Added problem describe

--- algorithms/medium/medium.go
+++ algorithms/medium/medium.go
@@ -1,6 +1,12 @@
 package medium
 
 // https://leetcode.com/problems/add-two-numbers/description/
+// You are given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order and each of their nodes contain a single digit.
+// Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
 
 // ListNode ...
 type ListNode struct {
@@ -38,7 +44,11 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-
+// Given a string, find the length of the longest substring without repeating characters.
+// Examples:
+// Given "abcabcbb", the answer is "abc", which the length is 3.
+// Given "bbbbb", the answer is "b", with the length of 1.
+// Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 func lengthOfLongestSubstring(s string) int {
 	sr := []rune(s)
 	n := len(sr)
