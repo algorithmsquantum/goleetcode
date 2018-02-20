ca575f16bb375d07c6a76a1e4923cfac8cf92752
Lingchao Xin <douglarek@gmail.com>
2018-02-20 22:37:35 +0800

    Single Number

--- algorithms/interview/array/easy.go
+++ algorithms/interview/array/easy.go
@@ -13,3 +13,21 @@ func removeDuplicates(nums []int) int {
 	}
 	return length
 }
+
+// Given an array of integers, every element appears twice except for one. Find that single one.
+// Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory ?
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+func singleNumber(nums []int) int {
+	m := make(map[int]int)
+	for _, v := range nums {
+		if _, ok := m[v]; !ok {
+			m[v] = v
+		} else {
+			delete(m, v)
+		}
+	}
+	for k := range m {
+		return k
+	}
+	return 0
+}
--- algorithms/interview/array/easy_test.go
+++ algorithms/interview/array/easy_test.go
@@ -22,3 +22,26 @@ func Test_removeDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"t1", args{[]int{1}}, 1},
+		{"t2", args{[]int{1, 2, 2}}, 1},
+		{"t3", args{[]int{1, 2, 3, 2, 4, 4, 3}}, 1},
+		{"t4", args{[]int{}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
